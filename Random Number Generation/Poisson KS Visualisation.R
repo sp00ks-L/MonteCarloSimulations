6c5489e0e3ef40f1028e54a92a33b20a30b634f4
Luke Jones <55932367+sp00ks-L@users.noreply.github.com>
2021-05-02 16:07:34 +0100

    Create Poisson KS Visualisation.R

---
+++ Random Number Generation/Poisson KS Visualisation.R
@@ -0,0 +1,77 @@
+# Title     : Visualisation of KS Stat for Poisson Distribution
+# Objective : Implement a graphical representation to show what the Kolmogorov-Smirnov stat is and how its calculated
+# Created by: Luke
+# Created on: 01/04/2021
+
+if (!exists("poisson_generator", mode = "function")) source("Sussex R/Final Code/Poisson/poisson_generator.R")
+
+library("ggplot2")
+# Extra imports for plot font, not required
+# library(extrafont)
+# loadfonts(device = "win")
+
+# N is relatively small to exacerbate and difference between the 2 ECDFs
+N <- 150
+lambda <- 120
+
+my_pois <- poisson_generator(N, lambda)
+inbuilt_pois <- rpois(N, lambda)
+
+f.a <- ecdf(inbuilt_pois)
+f.b <- ecdf(my_pois)
+
+# x <- create a sequence between min and max of the samples with length = N
+x <- seq(min(inbuilt_pois, my_pois), max(inbuilt_pois, my_pois), length.out = length(inbuilt_pois))
+x0 <- mean(x[which(abs(f.a(x) - f.b(x)) == max(abs(f.a(x) - f.b(x))))]) # find x coord for greatest distance between ECDFs
+y0 <- mean(f.a(x0)) # find y coordinates for line between ECDFs at our x0 coordinate
+y1 <- mean(f.b(x0))
+
+# Im sure there is a more elegant way of solving this, but this works
+inbuilt_data <- data.frame(
+  # Data for inbuilt function and setting colour/label for each
+  x = inbuilt_pois,
+  g = gl(2, N, labels = c("Theoretical Distribution", "Generated Variates"))
+)
+
+my_data <- data.frame(
+  # Data for my pois generator
+  xs = my_pois
+)
+
+ks_data <- data.frame(
+  # Data required to plot the line between the 2 ECDFs
+  xs = c(x0, x0),
+  ys = c(y0, y1),
+  xpair = c(x0, x0),
+  ypair = c(y0, y1)
+)
+
+
+ggplot(inbuilt_data, aes(x, colour = "R Poisson Generator")) +
+  stat_ecdf(geom = "step", alpha = 0.75) +
+  stat_ecdf(data = my_data, aes(xs, colour = "My Poisson Generator"), geom = "step", alpha = 0.75) +
+  geom_point(data = ks_data, aes(x = xs, y = ys, colour = "KS Stat"), shape = 10) +
+  geom_line(data = ks_data, aes(x = xpair, y = ypair, colour = "KS Stat"), size = 0.5, lineend = "round", linetype = "dashed") +
+  # This sets the color of the KS line and the 2 ECDFs
+  scale_colour_manual(name = NULL, values = c('black', '#4c72b0', '#c44e52')) +
+  labs(title = "A Visualisation of the KS Statistic", x = "X", y = "Pr(X <= x)", subtitle = "Illustrating the maximum distance between 2 ECDFs")
+# Add line below to alter font of plot
+# theme(text = element_text(size = 13, family = "CM Sans CE"))
+
+###############
+# File Output #
+###############
+
+# FILENAME <- "KS_test.pdf"
+# PATH <- "C:/Users/Luke/Sussex Code/Sussex R/Poisson Tests/Plot Images/PDFS/"
+# ggsave(
+#   FILENAME,
+#   plot = last_plot(),
+#   device = "pdf",
+#   path = PATH,
+#   scale = 1,
+#   width = NA,
+#   height = NA,
+#   units = c("in", "cm", "mm"),
+#   dpi = 300
+# )
